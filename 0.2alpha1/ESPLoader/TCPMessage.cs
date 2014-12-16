97a25393f00e64db5db0faf2d7929df144349e80
Jonathan Georgino <jonathan@binhoelectronics.com>
2014-12-15 16:37:13 -0800

    Added TCPServer and TCPMessage classes

---
+++ 0.2alpha1/ESPLoader/TCPMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ESPLoader
+{
+    class TCPMessage : EventArgs
+    {
+        public NetworkStream SenderClient { get; private set; }
+        public string Content { get; private set; }
+
+        public TCPMessage(NetworkStream client, string message)
+        {
+            SenderClient = client;
+            Content = message;
+        }
+    }
+}
